917bf4a79ae9b5450c1c2c221a986725cfe9eb3b
fierval <fierval@gmail.com>
2015-04-16 13:15:38 -0700

    Pattern frequences update

--- Lesson 1/patternFreq.fsx
+++ Lesson 1/patternFreq.fsx
@@ -2,29 +2,14 @@
 open System.Linq
 open System.Collections.Generic
 
-let patFreq (s : string) (pat : string) = 
-    let mutable count = 0
-    let mutable stop = false
-    let mutable i = 0
-    while not stop do
-        let j = (s.IndexOf(pat, i))
-        if j > 0 && i < s.Length - pat.Length then
-            i <- j + 1
-            count <- count + 1
-        else
-            stop <- true
-
-    count
-
 let patPos (s : string) (pat : string) = 
-    let mutable count = 0
-    let mutable stop = false
-    let pos = List<int>()
-    while not stop do
-        let j = if pos.Count > 0 then s.IndexOf(pat, pos.Last() + 1) else s.IndexOf(pat)
-        if j > 0 then
-            pos.Add(j)
-        else
-            stop <- true
+    0 |> 
+    Seq.unfold 
+        (fun state -> 
+            let i = s.IndexOf(pat, state)
+            if i < 0 then None else Some(i, i + 1)
+        )
+    |> Seq.toArray
 
-    pos.ToArray()
+let patFreq (s : string) (pat : string) = 
+    (patPos s pat).Length
