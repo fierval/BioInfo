86da88045e87ad0f20a6dbe5c76bc21431924cfd
fierval <fierval@gmail.com>
2015-04-18 15:32:34 -0700

    String generation for testing

---
+++ Lesson 1/stringGen.fsx
@@ -0,0 +1,7 @@
+﻿open System
+
+let generateString len =
+    let alphabet = ['A'; 'C'; 'G'; 'T']
+
+    let rnd = Random(int DateTime.Now.Ticks)
+    String(len |> Seq.unfold (fun state -> if state = 0 then None else Some(alphabet.[rnd.Next(alphabet.Length)], state - 1)) |> Seq.toArray)
