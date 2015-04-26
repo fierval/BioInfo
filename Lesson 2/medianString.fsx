10cabb353b5571e925b8ef553f1caccbe70dc9a2
fierval <fierval@gmail.com>
2015-04-25 21:11:28 -0700

    Finding motifs

---
+++ Lesson 2/medianString.fsx
@@ -0,0 +1,38 @@
+﻿#load @"..\Lesson 1\maxMutation.fsx"
+
+open System.IO
+open System.Linq
+open System.Collections.Generic
+open MaxMutation
+open FuzzyMatch
+open System
+
+Environment.CurrentDirectory <- @"C:\Users\Boris\Downloads"
+
+/// <summary>
+/// generate all k-mers from the ititial seed s
+/// </summary>
+/// <param name="s"></param>
+let generateKmers k =
+    let s = String('A', k)
+    let initial = kMersMutat s k k
+    seq{yield s; yield! getMutations initial k} |> Seq.toArray
+    
+let findMedianBrute (dna : string []) k =
+    let d (s : string) (pat : string) =
+        [0..s.Length-pat.Length].AsParallel().Select(fun i -> hammingDist (s.Substring(i, pat.Length)) pat)
+            .Min()
+    
+    let dist (dna : string []) pat =
+        dna.AsParallel().Select(fun s -> d s pat).Aggregate(0, fun state dst -> state + dst)
+
+    let kmers = generateKmers k
+
+    let distances = kmers |> Array.map (fun pat -> dist dna pat) |> Seq.zip kmers
+    let minTuple = distances.GroupBy(fun (km, dist) -> dist).OrderBy(fun gr -> gr.Key).First()
+    fst (minTuple.Single())
+        
+let findMedianBruteFile fileName =
+    let inp = File.ReadAllLines(fileName)
+    let k = int (inp.[0].Trim())
+    findMedianBrute inp.[1..] k
---
+++ Lesson 2/motifEnum.fsx
@@ -0,0 +1,70 @@
+﻿#load @"..\Lesson 1\maxMutation.fsx"
+
+open System.IO
+open System.Linq
+open System.Collections.Generic
+open MaxMutation
+open FuzzyMatch
+
+/// <summary>
+/// Brute force finding of k-mers with d-mutations
+/// </summary>
+/// <param name="dna"></param>
+/// <param name="k"></param>
+/// <param name="d"></param>
+let findKMersBrute (dna : string []) k d =
+    // per string, get all the mutations that appear at least once in each string
+    let init = 
+        (seq {
+            for s in dna do
+                let initial = kMersMutat s k d
+                let mutations = (getMutations initial d).Distinct().Except(initial.Keys)
+
+                // not run the mutations
+                let countsMut = 
+                    mutations
+                        .ToDictionary((fun x -> x),(fun x -> countD s x d))
+                        .Where(fun kvp -> kvp.Value > 0)
+                        .Select(fun kvp -> kvp.Key)
+
+                let all = countsMut.Union(initial.Keys).ToArray()
+                yield! all
+       }).Distinct()
+    
+    // get only those that appear in all strings
+    let kmers =
+        init.AsParallel().Where(fun km -> dna.AsParallel().Select(fun s -> countD s km d).Count(fun c -> c > 0) = dna.Length)
+    kmers.ToArray()
+
+let findKmersBruteFile fileName =
+    let inp = File.ReadAllLines(fileName)
+    let kd = inp.[0].Trim().Split([|' '|], 2)
+    let k, d = int (kd.First()), int (kd.Last())
+    let res = findKMersBrute inp.[1..] k d
+    File.WriteAllLines(@"c:\temp\sol21.txt", res)
+
+
+let dna1 = [|
+            "ATTTGGC"
+            "TGCCTTA"
+            "CGGTATC"
+            "GAAAATT"
+            |]
+
+let k, d = 3, 1
+
+// test on an extra dataset
+let fileName = @"c:\temp\extra.txt"
+findKmersBruteFile fileName
+
+let expected = @"c:\temp\expected.txt"
+let actual = @"c:\temp\sol21.txt"
+
+let comp (exp : string []) (act : string []) =
+    if exp.Length <> act.Length then failwith "Different counts"
+    let expSet = HashSet(exp)
+    if act.Except(exp).Union(exp.Except(act)).Count() > 0 then failwith "Different sets"
+
+
+let exp = File.ReadAllText(expected).Split([|'\r'; '\n'; ' '|])
+let act = File.ReadAllLines(actual)
