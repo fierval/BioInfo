d2acf06865c3fda12669a245b004e4bf532b6e43
fierval <fierval@gmail.com>
2015-07-24 19:36:15 -0700

    Solving # of peptides of mass m

--- Building Antibiotics/2d-.fsx
+++ Building Antibiotics/2d-.fsx
@@ -2,13 +2,11 @@
 #load "DataStructs.fsx"
 
 open System.Linq
-open System.Collections.Generic
 open System
-open System.IO
 open DataStructs
 
 // array of amino acid weights
-let weights = aminoAcidOneLetterIntegerMass |> Seq.map (fun kvp -> kvp.Value) |> Seq.toArray
+let weights = (aminoAcidOneLetterIntegerMass |> Seq.map (fun kvp -> kvp.Value)).Distinct() |> Seq.toArray
 
 /// Count then number of peptides of mass m
 let countNumPeptides m =
@@ -17,24 +15,21 @@ let countNumPeptides m =
         let allCounts : int64 [] = Array.zeroCreate (max m weights.[weights.Length - 1] + 1)
         
         // initialize dynamic programming store
-        Array.ForEach(weights, fun w -> allCounts.[w] <- allCounts.[w] + 1L)
+        Array.ForEach(weights, fun w -> allCounts.[w] <- 1L)
         let dynamicProgStep n =
-            let filtered = weights |> Array.filter (fun w -> n - w >= weights.[0]) 
+            let filtered = weights |> Array.filter (fun w -> (n - w) >= weights.[0]) 
             if filtered.Length > 0 then
-                let res = int64 (filtered
+                int64 (filtered
                     |> Array.map (fun w -> allCounts.[n - w])
                     |> Array.sum)
-                if res > 0L then
-                    printfn "%d %A %d" n (Array.map (fun w -> allCounts.[n - w]) filtered) (res + allCounts.[n])
-                res
             else
                 0L
 
+        // equivalent to a simple loop, but we are using tail recursion
         let rec fillDynArray n =
             if n > m then allCounts.[m]
             else
                 allCounts.[n] <- allCounts.[n] + dynamicProgStep n
-                //printfn "%d %d" n allCounts.[n]
                 fillDynArray (n + 1)
 
         fillDynArray weights.[0]               
