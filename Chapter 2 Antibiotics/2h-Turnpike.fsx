d0ec191d5d551d6bb123f5b72eb4f59952ef1751
fierval <fierval@gmail.com>
2015-09-02 17:18:18 -0700

    Code complete Turnpike

--- Chapter 2 Antibiotics/2h-Turnpike.fsx
+++ Chapter 2 Antibiotics/2h-Turnpike.fsx
@@ -7,9 +7,7 @@ open ``2e-Cyclospectrum``
 
 type DecisionTree =
     | Empty
-    | TreeNode of deltas : Dictionary<int, int> * solution : int list * maxElem : DecisionTree * maxDist : DecisionTree * prev : DecisionTree * visited : int
-
-let isNonEmptyDict (dct : Dictionary<int, int>) = dct <> Unchecked.defaultof<Dictionary<int, int>>
+    | TreeNode of deltas : Dictionary<int, int> * solution : int list * maxElem : DecisionTree * maxDist : DecisionTree * prev : DecisionTree 
 
 let keySeqMax (ds : Dictionary<int, int>) = ds.Keys |> Seq.max
 
@@ -28,47 +26,58 @@ let stepOk elem res deltas =
     let distances = (elem :: res) |> Seq.map (fun r -> abs (elem - r)) |> Seq.toList
     if isIn deltas distances then distances else []
 
-let visit =
+let isEmpty = 
+    function
+    | Empty -> true
+    | _ -> false
+
+let getPrev =
     function
     | Empty -> Empty
-    | TreeNode(dct, sol, maxElem, maxDist, prev, visited) -> 
-        TreeNode(dct, sol, maxElem, maxDist, prev, visited + 1)
+    | TreeNode (deltas, res, maxElem, maxDist, prev) -> prev
 
 let rec insert (deltas : Dictionary<int, int>) (res : int list) (node : DecisionTree) (prevNode : DecisionTree) maxSol =
 
     match node with 
-    | Empty -> TreeNode(deltas, res, Empty, Empty, prevNode, 0)
-    | TreeNode(dct, rslt, maxElem, maxDist, prev, visited) as cur ->
-        if visited < 2 then
-            let elem = if visited = 0 then keySeqMax deltas else maxSol - keySeqMax deltas
+    | Empty -> TreeNode(deltas, res, Empty, Empty, prevNode)
+    | TreeNode(dct, rslt, maxElem, maxDist, prev) as cur ->
+        if isEmpty maxElem || isEmpty maxDist then
+            let elem = if isEmpty maxElem then keySeqMax deltas else maxSol - keySeqMax deltas
             let dists = stepOk elem res deltas
             if dists.Length > 0 then
                 let newDeltas = removeDist deltas dists
-                if visited = 0 then
+                if isEmpty maxElem then
                     insert newDeltas (elem::res) maxElem cur maxSol
                 else 
                     insert newDeltas (elem::res) maxDist cur maxSol
             else
-                let visitedTree = visit cur
-                insert deltas res visitedTree prev maxSol
+                insert deltas res prev (getPrev prev) maxSol
         else
-            let prevPrev =
-                match prev with
-                | Empty -> Empty
-                | TreeNode(dct, rslt, maxElem, maxDist, prev, visited) -> prev
-            insert deltas res prev prevPrev maxSol
+            insert deltas res prev (getPrev prev) maxSol
 
 let turnpike (dA : int seq) =
     //hashset of ditance -> # times appearing
     let deltas = dA |> Seq.filter (fun d -> d > 0) |> dictOfAminos
     deltas.Add(0, 1)
     let maxSol = keySeqMax deltas
-    let deltaLength (dct : IDictionary<'T, 'U>) = dct.Values |> Seq.sum
-    let origLength = deltaLength deltas
 
-    let buildSolution (deltas : Dictionary<int, int>) (res : int list) =
-        if deltas.Count()
+    let rec buildSolution (deltas : Dictionary<int, int>) (res : int list) (node : DecisionTree) (prev : DecisionTree) =
+        if deltas.Count = 0 then res
+        else
+            let newNode = insert deltas res node prev maxSol
+            let prev = node
+            match newNode with
+            | Empty -> [] // no solution
+            | TreeNode(deltas, res, maxElem, maxDist, prev) ->
+                if not (isEmpty maxElem) && not (isEmpty maxDist) && isEmpty prev then [] // came all the way back, no solution
+                else
+                    buildSolution deltas res newNode prev
 
-    
+    // validate that the length of the diffs set contains just the right number of entries
+    let origLength = deltas.Values |> Seq.sum
+    let solLength = int (floor(1. + sqrt(1. + (8. * float origLength)))/ 2.)
+    if (solLength - 1) * solLength / 2 <> origLength then []
+    else
+        buildSolution deltas [0] Empty Empty
 
     
