c70372c0f6ed984e4695a6b646e9c2d61360e980
fierval <fierval@gmail.com>
2015-09-07 17:25:56 -0700

    optimized cyclopeptide solution

--- Chapter 2 Antibiotics/cyclospectrum.fsx
+++ Chapter 2 Antibiotics/cyclospectrum.fsx
@@ -8,6 +8,7 @@
 
 open System.Linq
 open System
+open System.Collections.Generic
 open System.IO
 open DataStructs
 open FsUnit
@@ -23,6 +24,7 @@ Alea.CUDA.Settings.Instance.Resource.Path <- Path.Combine(__SOURCE_DIRECTORY__, 
 // array of amino acid weights
 let weights = (aminoAcidOneLetterIntegerMassTrunc |> Seq.map (fun kvp -> kvp.Value)) |> Seq.toArray
 
+// brute force O(n^3) solution
 let cyclospec (peptide : int seq) =
     let len = peptide.Count()
     let pepArr = peptide |> Seq.toArray
@@ -43,6 +45,24 @@ let cyclospec (peptide : int seq) =
                         parsum    
                         )
     }) |> Seq.sort |> Seq.toArray
+
+// O(n^2) solution
+let cyclospecOpt (peptide : int seq) =
+    let len = peptide.Count()
+    let pepArr = peptide |> Seq.toArray
+
+    let output = Array.zeroCreate ((len - 1) * len)
+
+    for i = 0 to len - 2 do
+        [0..len-1] 
+            |> Seq.iter 
+                (fun j ->  
+                    let ind = i + j
+                    output.[i * len + j] <- 
+                        if i = 0 then pepArr.[j] 
+                        else output.[(i - 1) * len + j] + pepArr.[if ind >= len then ind - len else ind]
+                    )
+    seq {yield 0; yield pepArr |> Array.sum; yield! output} |> Seq.sort |> Seq.toArray
 
 // generates a peptide for comparison
 let generatePeptide (len : int) =
@@ -83,9 +103,11 @@ let cyclospecGpu (peptide : int []) =
 let test len =
     let peptide = generatePeptide len
     let cpu = cyclospec peptide
+    let cpuOpt = cyclospecOpt peptide
     let gpu = cyclospecGpu peptide
 
     should equal cpu gpu
+    should equal cpu cpuOpt 
 
 // experiment: run from 10 ** low to 10 ** high array length
 let experiment low high =
@@ -97,12 +119,13 @@ let experiment low high =
 
     let sw = Stopwatch()
     let cpuTimes = Array.zeroCreate (high - low + 1)
+    let cpuOptTimes = Array.zeroCreate (high - low + 1)
     let gpuTimes = Array.zeroCreate (high - low + 1)
-
 
     for i = low to high do
         let range = 500 * i
         let arr = generatePeptide range
+        let idx = i - low
 
         // Run on CPU
         sw.Restart()
@@ -111,10 +134,16 @@ let experiment low high =
         let h1 = cyclospec arr
         sw.Stop()
 
-        let idx = i - low
-
         cpuTimes.[idx] <- range, sw.Elapsed.TotalSeconds
         printfn "Computed on CPU: %0.5f sec" (snd cpuTimes.[idx])
+
+        // Run on CPU (optimized)
+        sw.Restart()
+        let h3 = cyclospecOpt arr
+        sw.Stop()
+
+        cpuOptTimes.[idx] <- range, sw.Elapsed.TotalSeconds
+        printfn "Computed on CPU (optimized): %0.5f sec" (snd cpuOptTimes.[idx])
 
         //Run on GPU
         sw.Restart()
@@ -124,14 +153,12 @@ let experiment low high =
         printfn "Computed on GPU: %0.5f sec" (snd gpuTimes.[idx])
         printfn ""
 
-        // make sure we are ok
-        should equal h1 h2
-
     Chart.Combine(
         [Chart.Line(cpuTimes, Name="CPU")
+         Chart.Line(cpuOptTimes, Name="CPU (optimized)")
          Chart.Line(gpuTimes, Name="GPU")
         ] 
     )
-        .WithYAxis(Log=false, Title = "sec")
+        .WithYAxis(Log=true, Title = "sec")
         .WithXAxis(Min=500. * float low, Title = "length")
-        .WithLegend(InsideArea=true) 
+        .WithLegend(InsideArea=false) 
