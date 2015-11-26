51c3e23dfcd6db6fabbd53657ad1393cb2eef6c8
fierval <fierval@gmail.com>
2015-11-25 20:15:22 -0800

    simplifying Euler cycle fst drop

--- Assembling DNA/3f-EulerCycle.fsx
+++ Assembling DNA/3f-EulerCycle.fsx
@@ -3,61 +3,75 @@ open System.Linq
 open System.IO
 open System.Collections.Generic
 
+#load @"..\Chapter 3 Molecular Clocks\environment.fsx"
+
 type 'a Euler = Dictionary<'a, List<'a>>
 type 'a Edge = 'a * 'a
-type 'a Cycle = Dictionary<'a, 'a>
-
-// how to follow the cyclic dictionary
-type 'a Index = Dictionary<'a, int>
+type 'a Cycle = List<'a>
 
 let parse (s : string []) : int Euler =
     let undec = 
         s 
         |> Array.map(fun e -> 
-                        let arr = e.Split([|" "; "->"|], 2, StringSplitOptions.RemoveEmptyEntries)
-                        int arr.[0], arr.[2].Split(',').Select(fun el -> int (el.Trim())).ToList())
+                        let arr = e.Split([|"->"|], 2, StringSplitOptions.RemoveEmptyEntries)
+                        int (arr.[0].Trim()), arr.[1].Split(',').Select(fun el -> int (el.Trim())).ToList())
 
     undec.ToDictionary(fst, snd)
 
 let findCycles (graph : 'a Euler) =
-    let rec buildCycle (cycle : 'a Cycle) (curNode : 'a) (nodesUnusedEdges : HashSet<'a>) (index : 'a Index) =
+    let rec findCyclesRec (curCycle : 'a Cycle)  =
 
-        // get new node for the cycle:
-        // remove it from the graph
-        // keep track of unused edges
-        let getEdge node =
-            let nextNode = graph.[node].[0]
-            cycle.Add(node, nextNode)
-            graph.[node].RemoveAt(0)
-            // we don't care about adding existing items to a hash set. It checks for them
-            if graph.[node].Count = 0 
-                then graph.Remove node |> ignore
-                else nodesUnusedEdges.Add node |> ignore
-            nextNode
+        let rec buildCycle (cycle : 'a Cycle)  =
+        
+            // get new node for the cycle:
+            // remove it from the graph
+            // keep track of unused edges
+            let (|NextNode|_|) node =
+                if graph.ContainsKey node then
+                    let nextNode = graph.[node].[0]
+                    cycle.Add(nextNode)
+                    graph.[node].RemoveAt(0)
+                    // we don't care about adding existing items to a hash set. It checks for them
+                    if graph.[node].Count = 0 
+                        then graph.Remove node |> ignore
+                    Some nextNode
+                else
+                    None
 
-        let nextNode = getEdge curNode
-        index.Add(curNode, index.Count)
-        if nextNode = curNode then cycle else buildCycle cycle nextNode nodesUnusedEdges index
-    
-    let rec findCyclesRec (curCycle : 'a Cycle) (nodesUnusedEdges : HashSet<'a>) (index : 'a Index) =
+            match curCycle.Last() with
+                | NextNode nextNode -> buildCycle cycle
+                | _ -> cycle.RemoveAt(cycle.Count - 1); cycle
+                
+
         // rearrange the index so that we 
         // have walked the entire index from the start node
-        let rearrangeCycleIndex startNode =
-            let mutable nextNode = startNode
-            for i = 0 to curCycle.Count do
-                index.[nextNode] <- i
-                nextNode <- curCycle.[nextNode]
-                
+        let rearrangeCycleIndex curNode =
+            let start = curCycle.IndexOf(curNode)
+            let move = curCycle.GetRange(0, start)
+            curCycle.AddRange move
+            curCycle.RemoveRange(0, start)
+            curCycle.Add curNode
+
+        let curNode = curCycle.Last()
         if curCycle.Count > 0 then 
-            let startNode = nodesUnusedEdges.First()
             // walk the cycle starting from the new edge
-            rearrangeCycleIndex startNode
+            rearrangeCycleIndex curNode
             // all unused edges were used
-            if not (graph.ContainsKey startNode) then (nodesUnusedEdges.Remove startNode |> ignore)
 
-        let cycle = buildCycle curCycle (nodesUnusedEdges.First()) nodesUnusedEdges index
-        if graph.Count = 0 then (cycle, index) else findCyclesRec cycle nodesUnusedEdges index
+        let cycle = buildCycle curCycle
+        if graph.Count = 0 then cycle else findCyclesRec cycle
 
-    let initUnused = HashSet<'a>()
-    initUnused.Add(graph.First().Key) |> ignore
-    findCyclesRec (Dictionary<'a, 'a>()) initUnused (Dictionary<'a, int>())
+    let curCycle = List<'a>()
+    curCycle.Add (graph.First().Key)
+    findCyclesRec curCycle
+
+let name = "euler1.txt"
+
+let solve name =
+    let strs = File.ReadAllLines name
+    let graph = parse strs
+
+    let curCycle = List<int>()
+    let cycle = curCycle
+
+    findCycles graph
