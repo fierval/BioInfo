18c90eb7b253f3098a021cacee4101505eec470a
fierval <fierval@gmail.com>
2015-12-18 18:41:03 -0800

    Fix string path

--- Facebook/matrixStringPath.fsx
+++ Facebook/matrixStringPath.fsx
@@ -2,13 +2,13 @@
 open System.Collections.Generic
 open System.Linq
 
-let findNextPos i j (positions : List<int*int>) (visited : bool [,]) =
+let findNextPos i j (positions : List<int*int>) (visited : HashSet<int*int>) =
     let mutable stop = false
     let mutable k = 0
     let res = List<int>()
     while k < positions.Count do
         let x, y = positions.[k]
-        if (x = i - 1 && y = j) || (x = i + 1 && y = j) || (x = i && y = j + 1) || (x = i && y = j - 1) && not visited.[x, y] then res.Add(k)
+        if (x = i - 1 && y = j) || (x = i + 1 && y = j) || (x = i && y = j + 1) || (x = i && y = j - 1) && not (visited.Contains((x, y))) then res.Add(k)
         k <- k + 1
     res
 
@@ -17,7 +17,9 @@ let walkPath (matr : char [,]) (str : string) =
     let len2 = Array2D.length2 matr
     let path = List<int*int>()
     let startMap = Dictionary<char, List<int * int>>()
-    let visited = Array2D.create len1 len2 false
+    let visited = HashSet<int*int>()
+    let depths = Stack<int>()
+    
     matr 
     |> Array2D.iteri 
         (fun i j s -> 
@@ -29,30 +31,38 @@ let walkPath (matr : char [,]) (str : string) =
     if chars.Count(fun c -> not (startMap.ContainsKey c)) > 0 then failwith "no path"
 
     let mutable k = 0
+    let curVisited = List<int*int>()
 
     while k < chars.Length do
         let positions = startMap.[chars.[k]]
         if k = 0 then
-            let i, j = positions.[0]
-            path.Add(i,j)
-            positions.RemoveAt(0)
             positions |> Seq.iter (fun pos -> toVisit.Push pos)
-            visited.[i,j] <- true
-        else
-            let curI, curJ = path.Last()
-            let nextPos = findNextPos curI curJ positions visited
+            depths.Push(toVisit.Count)
+            k <- k + 1
+                    
+        //get next position from the stack
+        let i, j = toVisit.Pop()
+        path.Add(i, j)
+        if k < chars.Length - 1 then
+            let nextPos = findNextPos i j positions visited
+            visited.Add(i, j) |> ignore
+            curVisited.Add(i,j)
+
             if nextPos.Count > 0 then
-                let i, j = positions.[nextPos.[0]]
-                path.Add(i,j)
-                visited.[i, j] <- true
-                nextPos.RemoveAt(nextPos.[0])
                 nextPos |> Seq.iter (fun pos -> toVisit.Push positions.[pos])
+                depths.Push(nextPos.Count)
                 k <- k + 1
             else
                 if toVisit.Count = 0 then failwith "path does not exist"
                 else
                     path.RemoveAt(path.Count - 1)
-                    toVisit.Pop() |> ignore
+                    depths.Push(depths.Pop() - 1)
+                    if depths.Peek() = 0
+                    then
+                        depths.Pop() |> ignore
+                        for i, j in curVisited do
+                            visited.Remove(i, j) |> ignore
+                            curVisited.Clear()
                     k <- k - 1
 
 let parseStrs (strs : string seq) =
