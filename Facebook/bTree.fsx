15218530015d2141ce17418f380ab2b70870e779
fierval <fierval@gmail.com>
2015-12-21 16:35:44 -0800

    walk sorted

--- Facebook/bTree.fsx
+++ Facebook/bTree.fsx
@@ -4,30 +4,34 @@ type 'a Tree =
 | Empty
 | Node of left : 'a Tree * value : 'a * right : 'a Tree
 
-let rec insert (tree : 'a Tree) (value : 'a) =
+let rec insert (value : 'a) (tree : 'a Tree) =
     match tree with
     | Empty -> Node(Empty, value, Empty)
-    | Node (left, v, right) -> if value < v then Node((insert left value), v, right) else Node(left, v, (insert right value))
+    | Node (left, v, right) -> if value < v then Node((insert value left), v, right) else Node(left, v, (insert value right))
 
-let walk (tree : 'a Tree) =
+let walkSorted (tree : 'a Tree) =
     let path = Stack<'a Tree>()
     let lst = List<'a>()
 
     let rec move (t : 'a Tree) isRight=
         match t with
         | Empty -> Empty
-        | Node (left, v, right) ->
-            if isRight then right else left
+        | Node (left, v, right) -> if isRight then right else left
 
     let getValue = function
         | Empty -> failwith "empty"
         | Node (_, v, _) -> v
+    
+    let hasRight = function
+    |Empty -> false
+    |Node(_, _, right) -> right <> Empty
 
     let rec keepMoving t =
         if t = Empty then t
         else
             let t = move t false
-            path.Push t
+            if t <> Empty then 
+                path.Push t
             keepMoving t
 
     let rec createPath tree =
@@ -35,17 +39,27 @@ let walk (tree : 'a Tree) =
         else
             match tree with
             | Empty -> lst
-            | Node(left, v, right) ->
+            | _ ->
                 while keepMoving tree <> Empty do
                     ignore true
-
-                lst.Add(path.Pop() |> getValue)
-
-                let tree = path.Pop()
-                lst.Add(getValue tree)
                 
-                createPath (move tree true)
+                let tree = path.Pop()
+                lst.Add(tree |> getValue)
+                let tree = 
+                    if hasRight tree then
+                        tree
+                    else
+                        lst.Add(path.Peek() |> getValue)
+                        path.Pop()
+
+                let tree = move tree true
+                path.Push tree
+                createPath tree
+
 
     path.Push tree       
-    createPath tree        
+    createPath tree |> Seq.toArray
 
+let tree = insert 20 Empty|> insert 35 |> insert 15 |> insert 21 |> insert 17 |> insert 25 |> insert 13
+let path = Stack<int Tree>()
+let lst = List<int>()
