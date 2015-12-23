93ef8c999495b601d995fce8be0f2f3d109e84f0
fierval <fierval@gmail.com>
2015-12-22 19:25:48 -0800

    Days 6, 7

---
+++ Advent Code/Day 6.fsx
@@ -0,0 +1,53 @@
+﻿open System.IO
+
+let name = @"c:\users\boris\downloads\input.txt"
+
+let lights = Array2D.create 1000 1000 false
+let newLights : int [,] = Array2D.zeroCreate 1000 1000
+
+type rect = {left: int; right: int; top: int; bottom: int}
+
+type Instruction =
+| On
+| Off
+| Toggle
+
+let parse (lines : string []) =
+
+    lines
+    |> Array.map (fun s -> 
+                    let parts = s.Trim().Split(' ')
+                    let isSmall = parts.Length = 4
+                    let i = if isSmall then 1 else 2
+                    let instr = if isSmall then Toggle elif parts.[1] = "on" then On else Off
+                    let [|left; top|]  = parts.[i].Split(',') |> Array.map int
+                    let [|right; bottom|] = parts.[i + 2].Split(',') |> Array.map int
+                    let r = {left = left; right = right; top = top; bottom = bottom}
+                    instr, r
+        )
+let setRange instr r =
+    for i = r.left to r.right do
+        for j = r.top to r.bottom do
+            lights.[i, j] <- 
+                match instr with
+                | On -> true
+                | Off -> false
+                | Toggle -> not lights.[i,j]
+
+let newSetRange instr r =
+    for i = r.left to r.right do
+        for j = r.top to r.bottom do
+            newLights.[i, j] <- 
+                match instr with
+                | On -> newLights.[i,j] + 1
+                | Off -> max 0 (newLights.[i,j] - 1)
+                | Toggle -> newLights.[i,j] + 2
+    
+let solve name =
+    File.ReadAllLines name |> parse
+    |> Seq.iter (fun instr -> newSetRange (fst instr) (snd instr))
+
+    seq {for i = 0 to 999 do
+            for j = 0 to 999 do
+                if newLights.[i,j] > 0 then yield newLights.[i,j]} |> Seq.sum
+    
---
+++ Advent Code/Day 7.fsx
@@ -0,0 +1,94 @@
+﻿open System.IO
+open System.Collections.Generic
+open System.Linq
+open System
+
+let name = @"c:\users\boris\downloads\input.txt"
+
+let channels = Dictionary<string, uint16>()
+
+[<FlagsAttribute>]
+type Op =
+| OR = 0
+| AND = 1
+| LSHIFT = 2
+| RSHIFT = 3
+
+type Expr =
+| LVAR of string
+| NOT of string
+| OP of Op * string * string
+
+let parse (instr : string) =
+    let [|left; right|] = instr.Split([|"->"|], StringSplitOptions.None) |> Array.map (fun s -> s.Trim())
+
+    let ast = Stack<Expr>()        
+
+    let tokens = left.Split(' ') |> Array.map (fun s -> s.Trim())
+    let lval = 
+        if tokens.[0] = "NOT" then
+            NOT(tokens.[1])
+        elif tokens.Length = 1 then
+            LVAR(tokens.[0])
+        else
+            OP(Enum.Parse(typeof<Op>, tokens.[1]) :?> Op, tokens.[0], tokens.[2])
+
+    right, lval
+            
+
+let rec exec right (instrs : Dictionary<string, Expr>) =
+    printfn "Solving for: %s with %A" right instrs.[right]
+    let left = instrs.[right]
+    
+    let getValue s =
+        let res, v = UInt16.TryParse(s)
+        if res then res, v
+        elif channels.ContainsKey s then
+            true, channels.[s]
+        else false, 0us
+    
+
+    match left with
+    | LVAR name -> 
+        let res, v = getValue name
+        if res then channels.Add(right, v); v
+        else exec name instrs
+    | NOT expr -> 
+        let res, v = getValue expr
+        if res then channels.Add(right, ~~~v); ~~~v
+        else ~~~(exec expr instrs)
+    | OP (op, e1, e2) ->
+        let res1, v1 = getValue e1
+        let res2, v2 = getValue e2
+        match op with
+            | Op.OR -> 
+                if res1 && res2 then channels.Add(right, v1 ||| v2); v1 ||| v2
+                else
+                    (if res1 then v1 else (exec e1 instrs)) ||| (if res2 then v2 else (exec e2 instrs))
+            | Op.AND  -> 
+                if res1 && res2 then channels.Add(right, v1 &&& v2); v1 &&& v2
+                else                
+                    (if res1 then v1 else (exec e1 instrs)) &&& (if res2 then v2 else (exec e2 instrs))
+            | Op.RSHIFT -> 
+                if res1 && res2 then channels.Add(right, v1 >>> int v2); v1 >>> int v2
+                else
+                    (if res1 then v1 else (exec e1 instrs)) >>> (if res2 then int v2 else int (exec e2 instrs))
+            | Op.LSHIFT -> 
+                if res1 && res2 then channels.Add(right, v1 <<< int v2); v1 <<< int v2
+                else
+                    (if res1 then v1 else (exec e1 instrs)) <<< (if res2 then int v2 else int (exec e2 instrs))
+        
+let solve key name =
+    exec key ((File.ReadAllLines name |> Array.map parse).ToDictionary(fst, snd))
+
+let solve2 keyOverride key name =
+    channels.Clear()
+    let overrideValue = solve key name    
+    channels.Clear()
+    let instrs = ((File.ReadAllLines name |> Array.map parse).ToDictionary(fst, snd))
+    instrs.Remove(keyOverride) |> ignore
+    channels.Add(keyOverride, overrideValue)
+    exec key instrs
+
+let key = "a"
+let keyOverride = "b"
