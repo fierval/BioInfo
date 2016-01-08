636ba85105995f8c3cdd23ed3ab5c53bf1aa82bb
fierval <fierval@gmail.com>
2016-01-07 19:55:40 -0800

    AoC, days 13 and 14

---
+++ Advent Code/Day 13.fsx
@@ -0,0 +1,68 @@
+﻿open System
+open System.Linq
+open System.IO
+open System.Collections.Generic
+let name = @"c:\users\boris\downloads\input.txt"
+
+type Sitting = Map<string * string, int>
+
+let distrib e L =
+    let rec aux pre post = 
+        seq {
+            match post with
+            | [] -> yield (L @ [e])
+            | h::t -> yield (List.rev pre @ [e] @ post)
+                      yield! aux (h::pre) t 
+        }
+    aux [] L
+
+let rec perms = function 
+    | [] -> Seq.singleton []
+    | h::t -> Seq.collect (distrib h) (perms t)
+
+let parse (strs : string []) : Sitting * string list =
+    let raw =
+        strs 
+        |> Array.map
+            (fun s ->
+                let words = s.Trim([|' '; '.'|]).Split(' ')
+                let key = words.[0], words.Last()
+                let val' = if words.[2] = "gain" then int words.[3] else -int words.[3]
+                key, val'
+            )
+    let map = raw |> Map.ofArray
+    let names = raw |> Array.toList |> List.map (fun ((a, b), c) -> [a; b]) |> List.concat |> List.distinct
+    map, names    
+
+
+let strs = File.ReadAllLines(name)
+
+let sittings (edges : Sitting) (names : string list) =
+    let permutes = perms names |> Seq.toList
+
+    permutes
+        |> List.map 
+            (fun permutation -> 
+                permutation
+                    |> List.windowed 2
+                    |> List.map (fun [a; b] -> [edges.[(a, b)]; edges.[(b,a)]])
+                    |> List.concat
+                    |> fun l -> edges.[permutation.[permutation.Length - 1], permutation.[0]] :: edges.[permutation.[0], permutation.[permutation.Length - 1]] :: l
+                    |> List.sum
+            )
+        |> List.max
+             
+let edges, names = parse strs
+sittings edges names
+
+(*Part 2*)
+let withMe (names : string list) (edges : Sitting) =
+    let namesMe =
+        names
+        |> List.map (fun e -> [((e, "Me"), 0); (("Me", e), 0)]) |> List.concat
+
+    let mapList = edges |> Map.toList
+    let fullEdges = [mapList; namesMe] |> List.concat |> Map.ofList
+    let fullNames = "Me"::names
+
+    sittings fullEdges fullNames
---
+++ Advent Code/Day 14.fsx
@@ -0,0 +1,44 @@
+﻿open System
+open System.Linq
+open System.IO
+open System.Collections.Generic
+let name = @"c:\users\boris\downloads\input.txt"
+
+let strs = File.ReadAllLines name
+
+type Ability = (int*int*int) []
+
+let parse (strs: string []) : Ability =
+    strs
+    |> Array.map
+        (fun s ->
+            let words = s.Trim().Split(' ')
+            (int words.[3]), (int words.[6]), (int words.[13])
+            )
+
+let distance speed duration rest total =
+    let totalJumps = total / (duration + rest)
+    let remains = total % (duration + rest)
+    totalJumps * speed * duration + (if remains > duration then duration * speed else remains * speed)
+
+let running (abils : Ability) total =
+    abils
+    |> Array.map (fun (speed, duration, rest) -> distance speed duration rest total)
+    |> Array.max
+
+let total = 2503
+running (parse strs) total
+
+let runningPoints (abils : Ability) total =
+    let rec points (acc : int []) rem =
+        if rem = total + 1 then Array.max acc
+        else
+            let dists = abils |> Array.map (fun (speed, duration, rest) -> distance speed duration rest rem)
+            let mDist = Array.max dists
+            let pts = dists |> Array.map (fun d -> if d = mDist then 1 else 0)
+            let acc = pts |> Array.map2 (fun a d -> a + d) acc
+            points acc (rem + 1)
+
+    points (Array.zeroCreate abils.Length) 1
+
+runningPoints (parse strs) total
