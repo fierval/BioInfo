5344dcc92983d0bb2c8632b68e029342e7c031ac
Boris Kogan <bernole@hotmail.com>
2016-01-23 20:22:23 -0800

    Day 20

---
+++ Advent Code/Day 20.fsx
@@ -0,0 +1,19 @@
+﻿(* from http://theburningmonk.com *)
+let findAllDivisors n =
+    let max = float n |> sqrt |> int
+    { 1..max } 
+    |> Seq.filter (fun n' -> n % n' = 0)
+    |> Seq.collect (fun n' -> [ n'; n / n' ])
+    |> Seq.filter (fun n' -> n/n' <= 50) // part 2
+    |> Seq.distinct
+    |> Seq.toArray
+
+let totalGifts = findAllDivisors >> Array.sumBy ((*) 11) // part 2 (10 - part1)
+
+let input = 36000000
+
+100000
+|> Seq.unfold (fun n -> Some(n+1, n+1))
+|> Seq.filter (fun n -> totalGifts n >= input)
+|> Seq.head
+
